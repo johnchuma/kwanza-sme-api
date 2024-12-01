f0ab17def9c117fc9535d1344fd7e83dd7c70cb7
johnchuma <johnvchuma@gmail.com>
2024-12-01 13:53:24 +0300

    enable filter

--- modules/metaCampaigns/metaCampaigns.controllers.js
+++ modules/metaCampaigns/metaCampaigns.controllers.js
@@ -111,7 +111,7 @@ const getUserMetaCampaigns = async (req, res) => {
       where: {
         [Op.and]: [
           {
-            id: {
+            name: {
               [Op.like]: `%${keyword}%`,
             },
           },
